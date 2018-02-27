2ed7762a772bb80e5868f6fc03ab0e2436df7851
Kailani <kkp.kailanipinon@gmail.com>
2018-02-27 15:43:02 -0800

    Health UI

---
+++ Assets/scripts/CameraFacingBillboard.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFacingBillboard : MonoBehaviour {
+
+    public Camera m_MainCam;
+
+    void Update() {
+        transform.LookAt(transform.position + m_MainCam.transform.rotation * Vector3.forward,
+            m_MainCam.transform.rotation * Vector3.up);
+    }
+}
